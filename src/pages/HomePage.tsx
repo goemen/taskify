095d3e873a36c4b08f87d7f66532c4f978cba74b
Goeme Nthomiwa <gnthomiwa@live.ca>
2021-07-01 12:13:44 -0700

    fixed floating action button [home/create task]

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -18,7 +18,7 @@ const useClasses = makeStyles((theme) => ({
     marginBottom: theme.spacing(2),
   },
   fab: {
-    position: "absolute",
+    position: "fixed",
     bottom: theme.spacing(2),
     right: theme.spacing(2),
   },
